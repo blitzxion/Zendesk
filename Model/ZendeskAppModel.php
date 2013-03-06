5018f1a05afe0c0f33c993c4dbeeaa217b9a104e
Ken Garland <ken@ufn.com>
2013-03-06 17:44:59 -0500

    adding basic app model

---
+++ Model/ZendeskAppModel.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * ZendeskApp model for Route53.
+ *
+ *
+ * PHP 5
+ *
+ * Copyright (c) Particle Ventures, LLC. (http://www.ufn.com)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Particle Ventures, LLC. (http://www.ufn.com)
+ * @link          https://github.com/unitedfloristnetwork/Route53 Cakephp Route53 Plugin
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+App::uses('AppModel', 'Model');
+class ZendeskAppModel extends AppModel {
+
+}
