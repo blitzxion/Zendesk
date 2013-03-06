4f78715287a245073f421fef43a12de1be85a2bd
Ken Garland <ken@ufn.com>
2013-03-06 17:45:11 -0500

    Adding Ticket model

--- Model/Ticket.php
+++ Model/Ticket.php
@@ -21,4 +21,71 @@ class Ticket extends ZendeskAppModel {
 
 	public $useDbConfig = 'Zendesk';
 
+	public function getList() {
+		$this->request = array('uri' => array('path' => 'tickets.json'));
+		return $this->find('all');
+	}
+
+	public function get($id) {
+		$this->request = array('uri' => array('path' => $id.'.json'));
+		return $this->find('all');
+	}
+
+	public function getRecent() {
+		$this->request = array('uri' => array('path' => 'recent.json'));
+		return $this->find('all');
+	}
+
+	public function getMany($ids = array()) {
+		$this->request = array(
+			'uri' => array(
+				'path' => 'show_many.json?ids={' . implode(',', $ids) . '}'
+			),
+			'method' => 'POST'
+		);
+		return $this->find('all');
+	}
+
+	public function create($data) {
+		$this->request = array(
+			'uri' => array('path' => 'tickets.json'),
+			'method' => 'POST',
+			'body' => $data
+			);
+		return $this->save($data);
+	}
+
+	public function update($id, $data) {
+		$this->request = array(
+			'uri' => array('path' => $id.'.json'),
+			'method' => 'PUT',
+			'body' => $data
+			);
+		return $this->update($data);
+	}
+
+	public function updateMany($ids, $data) {
+
+	}
+
+	public function markAsSpam($id) {
+
+	}
+
+	public function delete($id) {
+
+	}
+
+	public function deleteMany($ids) {
+
+	}
+
+	public function listCollaborators($id) {
+
+	}
+
+	public function listIncidents() {
+
+	}
+
 }
