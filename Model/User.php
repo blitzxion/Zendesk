026ec5fd2b6f69b2c433ddd42ad714fccb30aff4
Ken Garland <ken@ufn.com>
2013-03-06 20:13:55 -0500

    Beginning work on User model functions.

--- Model/User.php
+++ Model/User.php
@@ -17,8 +17,105 @@
 App::uses('ZendeskAppModel', 'Zendesk.Model');
 class User extends ZendeskAppModel {
 
+	/**
+	 * Model name
+	 * @var $name
+	 */
 	public $name = 'User';
 
+	/**
+	 * Database Configuration
+	 * @var string $useDbConfig
+	 */
 	public $useDbConfig = 'Zendesk';
 
+	/**
+	 * Retrieve a list of all users
+	 * @link http://developer.zendesk.com/documentation/rest_api/users.html#list-users
+	 * @return array
+	 */
+	public function getList() {
+		$this->request = array('uri' => array('path' => 'users.json'));
+
+		return $this->find('all');
+	}
+
+	/**
+	 * Retrieve a single user by id
+	 * @link http://developer.zendesk.com/documentation/rest_api/users.html#show-user
+	 * @param int $id
+	 * @return array
+	 */
+	public function get($id) {
+		if (!is_int($id)) {
+			throw new CakeException(__('User ID must be an integer. %s was given', gettype($id)));
+		}
+
+		$this->request = array('uri' => array('path' => $id.'.json'));
+
+		return $this->find('all');
+	}
+
+	/**
+	 * Create a new user
+	 * @link http://developer.zendesk.com/documentation/rest_api/users.html#create-user
+	 * @param array $data
+	 * @return array
+	 */
+	public function create(array $data) {
+		$this->request = array(
+			'uri' => array('path' => 'users.json'),
+			'method' => 'POST',
+			'body' => array('user' => $data)
+		);
+
+		return $this->save($data);
+	}
+
+	/**
+	 * Create several users (batch request)
+	 * @link http://developer.zendesk.com/documentation/rest_api/users.html#create-many-users
+	 * @param array $data
+	 * @return array
+	 */
+	public function createMany(array $data) {
+		$this->request = array(
+			'uri' => array(
+				'path' => 'create_many.json'
+			),
+			'method' => 'POST',
+			'body' => array('users' => $data) // users must be plural here
+			);
+
+		return $this->update($data);
+	}
+
+	/**
+	 * Update a specific user by id
+	 * @link http://developer.zendesk.com/documentation/rest_api/users.html#update-user
+	 * @param int $id
+	 * @param array $data
+	 * @return array
+	 */
+	public function updateUser($id, array $data) {
+		if (!is_int($id)) {
+			throw new CakeException(__('User ID must be an integer. %s was given', gettype($id)));
+		}
+		$this->request = array(
+			'uri' => array('path' => $id . '.json'),
+			'method' => 'PUT',
+			'body' => array('user' => $data)
+			);
+
+		return $this->update($data);
+	}
+
+	/**
+	 * Suspend a specific user by id
+	 * @param int $id
+	 * @return array
+	 */
+	public function suspend($id) {
+		return $this->updateUser(array('suspended' => true));
+	}
 }
