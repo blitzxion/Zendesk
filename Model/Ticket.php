2477a05d4cca0ddf7d61271191f7ead13b888f58
Ken Garland <ken@ufn.com>
2013-03-06 19:55:54 -0500

    Fleshing out remaining functions in Ticket model.

--- Model/Ticket.php
+++ Model/Ticket.php
@@ -17,75 +17,217 @@
 App::uses('ZendeskAppModel', 'Zendesk.Model');
 class Ticket extends ZendeskAppModel {
 
+	/**
+	 * Model name
+	 * @var $name
+	 */
 	public $name = 'Ticket';
 
+	/**
+	 * Database Configuration
+	 * @var string $useDbConfig
+	 */
 	public $useDbConfig = 'Zendesk';
 
+	/**
+	 * Retrieve a list of all tickets
+	 * @link http://developer.zendesk.com/documentation/rest_api/tickets.html#listing-tickets
+	 * @return array
+	 */
 	public function getList() {
 		$this->request = array('uri' => array('path' => 'tickets.json'));
+
 		return $this->find('all');
 	}
 
+	/**
+	 * Retrieve a single ticket by id
+	 * @link http://developer.zendesk.com/documentation/rest_api/tickets.html#getting-tickets
+	 * @param int $id
+	 * @return array
+	 */
 	public function get($id) {
+		if (!is_int($id)) {
+			throw new CakeException(__('Ticket ID must be an integer. %s was given', gettype($id)));
+		}
+
 		$this->request = array('uri' => array('path' => $id.'.json'));
+
 		return $this->find('all');
 	}
 
+	/**
+	 * Retrieve a the most recent tickets
+	 * @return array
+	 */
 	public function getRecent() {
 		$this->request = array('uri' => array('path' => 'recent.json'));
+
 		return $this->find('all');
 	}
 
-	public function getMany($ids = array()) {
+	/**
+	 * Retrieve a collection of multiple tickets by id
+	 * @example $this->Ticket->getMany(array(1,2,3,4));
+	 * @link http://developer.zendesk.com/documentation/rest_api/tickets.html#show-multiple-tickets
+	 * @param array $ids
+	 * @return array
+	 */
+	public function getMany(array $ids) {
 		$this->request = array(
 			'uri' => array(
 				'path' => 'show_many.json?ids={' . implode(',', $ids) . '}'
 			),
 			'method' => 'POST'
 		);
+
 		return $this->find('all');
 	}
 
-	public function create($data) {
+	/**
+	 * Create a new ticket
+	 * @link http://developer.zendesk.com/documentation/rest_api/tickets.html#creating-tickets
+	 * @param array $data
+	 * @return array
+	 */
+	public function create(array $data) {
 		$this->request = array(
 			'uri' => array('path' => 'tickets.json'),
 			'method' => 'POST',
-			'body' => $data
-			);
+			'body' => array('ticket' => array($data))
+		);
+
 		return $this->save($data);
 	}
 
-	public function update($id, $data) {
+	/**
+	 * Update a specific ticket by id
+	 * @link http://developer.zendesk.com/documentation/rest_api/tickets.html#updating-tickets
+	 * @param int $id
+	 * @param array $data
+	 * @return array
+	 */
+	public function update($id, array $data) {
+		if (!is_int($id)) {
+			throw new CakeException(__('Ticket ID must be an integer. %s was given', gettype($id)));
+		}
 		$this->request = array(
-			'uri' => array('path' => $id.'.json'),
+			'uri' => array('path' => $id . '.json'),
 			'method' => 'PUT',
-			'body' => $data
+			'body' => array('ticket' => array($data))
 			);
+
 		return $this->update($data);
 	}
 
-	public function updateMany($ids, $data) {
+	/**
+	 * Update several tickets by id (batch request)
+	 * @link http://developer.zendesk.com/documentation/rest_api/tickets.html#bulk-updating-tickets
+	 * @param array $ids
+	 * @param array $data
+	 * @return array
+	 */
+	public function updateMany(array $ids, array $data) {
+		$this->request = array(
+			'uri' => array(
+				'path' => 'update_many.json?ids={' . implode(',', $ids) . '}'
+			),
+			'method' => 'PUT',
+			'body' => array('ticket' => array($data))
+			);
 
+		return $this->update($data);
 	}
 
+	/**
+	 *
+	 * @link http://developer.zendesk.com/documentation/rest_api/tickets.html#mark-a-ticket-as-spam-and-suspend-the-requester
+	 * @param int $id
+	 * @return array
+	 */
 	public function markAsSpam($id) {
-
+		if (!is_int($id)) {
+			throw new CakeException(__('Ticket ID must be an integer. %s was given', gettype($id)));
+		}
 	}
 
+	/**
+	 * Delete a single ticket by id
+	 * @link http://developer.zendesk.com/documentation/rest_api/tickets.html#deleting-tickets
+	 * @param int $id
+	 * @return array
+	 */
 	public function delete($id) {
+		if (!is_int($id)) {
+			throw new CakeException(__('Ticket ID must be an integer. %s was given', gettype($id)));
+		}
 
+		$this->request = array(
+			'uri' => array(
+				'path' => $id . '.json'
+			),
+			'method' => 'DELETE'
+		);
+
+		return $this->delete($id);
 	}
 
-	public function deleteMany($ids) {
+	/**
+	 * Bulk Deleting Tickets
+	 * @link http://developer.zendesk.com/documentation/rest_api/tickets.html#bulk-deleting-tickets
+	 * @param array $ids
+	 * @return array
+	 */
+	public function deleteMany(array $ids) {
+		$this->request = array(
+			'uri' => array(
+				'path' => 'destroy_many.json?ids={' . implode(',', $ids) . '}'
+			),
+			'method' => 'DELETE'
+		);
 
+		return $this->delete($ids);
 	}
 
+	/**
+	 * List Collaborators for a Ticket
+	 * @link http://developer.zendesk.com/documentation/rest_api/tickets.html#list-collaborators-for-a-ticket
+	 * @param int $id
+	 * @return array
+	 */
 	public function listCollaborators($id) {
+		if (!is_int($id)) {
+			throw new CakeException(__('Ticket ID must be an integer. %s was given', gettype($id)));
+		}
 
+		$this->request = array(
+			'uri' => array(
+				'path' => $id . '/collaborators.json'
+			),
+			'method' => 'GET'
+		);
+
+		return $this->find('all');
 	}
 
-	public function listIncidents() {
+	/**
+	 * Listing Ticket Incidents
+	 * @link http://developer.zendesk.com/documentation/rest_api/tickets.html#listing-ticket-incidents
+	 * @param int $id
+	 * @return array
+	 */
+	public function listIncidents($id) {
+		if (!is_int($id)) {
+			throw new CakeException(__('Ticket ID must be an integer. %s was given', gettype($id)));
+		}
 
+		$this->request = array(
+			'uri' => array(
+				'path' => $id . '/incidents.json'
+			),
+			'method' => 'GET'
+		);
+
+		return $this->find('all');
 	}
-
 }
