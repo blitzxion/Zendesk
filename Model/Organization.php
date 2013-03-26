d2c3653f4e33bfe4940dcc1cd5d8b3621441e0fc
Ken Garland <kgarland@Kens-MacBook-Pro-2.local>
2013-03-26 10:53:09 -0400

    Adding functions to Organization model

--- Model/Organization.php
+++ Model/Organization.php
@@ -21,4 +21,105 @@ class Organization extends ZendeskAppModel {
 
 	public $useDbConfig = 'Zendesk';
 
+	/**
+	 * Retrieve a list of all organizations
+	 * @link http://developer.zendesk.com/documentation/rest_api/organizations.html#listing-organizations
+	 * @return array
+	 */
+	public function getList() {
+		$this->request = array('uri' => array('path' => 'organizations.json'));
+
+		return $this->find('all');
+	}
+
+	/**
+	 * Retrieve a single organization by id
+	 * @link http://developer.zendesk.com/documentation/rest_api/organizations.html#getting-organizations
+	 * @param int $id
+	 * @return array
+	 */
+	public function get($id) {
+		if (!is_int($id)) {
+			throw new CakeException(__('Org ID must be an integer. %s was given', gettype($id)));
+		}
+
+		$this->request = array('uri' => array('path' => $id.'.json'));
+
+		return $this->find('all');
+	}
+
+	/**
+	 * Create a new organization
+	 * @link http://developer.zendesk.com/documentation/rest_api/organizations.html#creating-organizations
+	 * @param array $data
+	 * @return array
+	 */
+	public function create(array $data) {
+		$this->request = array(
+			'uri' => array('path' => 'organizations.json'),
+			'method' => 'POST',
+			'body' => array('organization' => $data)
+		);
+
+		return $this->save($data);
+	}
+
+	/**
+	 * Create several organizations (batch request)
+	 * @link http://developer.zendesk.com/documentation/rest_api/organizations.html#create-many-organizations
+	 * @param array $data
+	 * @return array
+	 */
+	public function createMany(array $data) {
+		$this->request = array(
+			'uri' => array(
+				'path' => 'create_many.json'
+			),
+			'method' => 'POST',
+			'body' => array('organizations' => $data) // users must be plural here
+			);
+
+		return $this->update($data);
+	}
+
+	/**
+	 * Update a specific organization by id
+	 * @link http://developer.zendesk.com/documentation/rest_api/organizations.html#updating-organizations
+	 * @param int $id
+	 * @param array $data
+	 * @return array
+	 */
+	public function updateOrganization($id, array $data) {
+		if (!is_int($id)) {
+			throw new CakeException(__('Org ID must be an integer. %s was given', gettype($id)));
+		}
+		$this->request = array(
+			'uri' => array('path' => $id . '.json'),
+			'method' => 'PUT',
+			'body' => array('organization' => $data)
+			);
+
+		return $this->update($data);
+	}
+
+	/**
+	 * Delete a single organization by id
+	 * @link http://developer.zendesk.com/documentation/rest_api/organizations.html#deleting-organizations
+	 * @param int $id
+	 * @return array
+	 */
+	public function deleteOrganization($id) {
+		if (!is_int($id)) {
+			throw new CakeException(__('Org ID must be an integer. %s was given', gettype($id)));
+		}
+
+		$this->request = array(
+			'uri' => array(
+				'path' => $id . '.json'
+			),
+			'method' => 'DELETE'
+		);
+
+		return $this->delete($id);
+	}
 }
